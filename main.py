8240a97e697f38d9e85cba7797ef30333f51f39b
tha7samurai <tha7samurai@gmail.com>
2025-10-24 13:45:13 -0600

    A3

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from circleshape import *
 def main():
     pygame.init()
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     ship = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(60) / 1000
+        ship.update(dt)
         screen.fill('black')
         ship.draw(screen)
         pygame.display.flip()
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
