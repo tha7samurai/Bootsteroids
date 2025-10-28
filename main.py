8e9159e518c128fe0469371aed79f83d9bb24ecc
tha7samurai <tha7samurai@gmail.com>
2025-10-27 18:49:27 -0600

    A4

--- main.py
+++ main.py
@@ -2,21 +2,31 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from AsteroidField import *
 
 def main():
     pygame.init()
     screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     ship = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    AstField = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
-        ship.update(dt)
+        updatable.update(dt)
         screen.fill('black')
-        ship.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
 
