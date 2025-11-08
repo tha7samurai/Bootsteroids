6b6c0585651d2cb8f939c0708cd50ab8266a799e
tha7samurai <tha7samurai@gmail.com>
2025-11-08 13:24:25 -0700

    Revert "A5"
    This reverts commit e605c552d18d79024aa47c10e58547d683deec28.

--- circleshape.py
+++ circleshape.py
@@ -20,7 +20,3 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
-
-    def collides_with(self, other):
-        distance = self.position.distance_to(other.position)
-        return distance <= (self.radius + other.radius)
--- player.py
+++ player.py
@@ -1,13 +1,11 @@
 import pygame
 from circleshape import *
 from constants import *
-from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation=0
-        self.ShotCD=0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,8 +22,6 @@ class Player(CircleShape):
         self.rotation+=PLAYER_TURN_SPEED*dt
 
     def update(self, dt):
-        self.ShotCD-=max(dt,0)
-        self.ShotCD = max(self.ShotCD, 0)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -35,19 +31,7 @@ class Player(CircleShape):
             self.move(+dt)
         if keys[pygame.K_s]:
             self.move(-dt)
-        if keys[pygame.K_SPACE]:
-            self.shoot()
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-
-    def shoot(self):
-            if self.ShotCD > 0:
-                return False
-            self.ShotCD=PLAYER_SHOOT_COOLDOWN_SECONDS
-            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-            velocity=pygame.Vector2(0,1)
-            velocity=velocity.rotate(self.rotation)
-            velocity=velocity*PLAYER_SHOOT_SPEED
-            shot.velocity=velocity
